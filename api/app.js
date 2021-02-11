e84f9c2e1fc26b42d5611cce50f05cfca136250f
apocalypse-suite <mitchenko.catherine@gmail.com>
2021-02-11 14:05:58 +0200

    enable CORS

--- api/app.js
+++ api/app.js
@@ -10,6 +10,13 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+//Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //Getting all the lists
 app.get('/lists', (req, res) => {
   List.find({}).then((lists) => {
