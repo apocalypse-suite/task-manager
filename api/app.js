45e9f9a35736ba06b17bfe0a08d1f213445d74a0
apocalypse-suite <mitchenko.catherine@gmail.com>
2021-02-12 19:00:54 +0200

    solved CORS issue

--- api/app.js
+++ api/app.js
@@ -7,103 +7,99 @@ const {mongoose} = require('./db/mongoose');
 const {List, Task} = require('./db/models');
 
 const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
 
 app.use(bodyParser.json());
 
-//Enable CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 //Getting all the lists
 app.get('/lists', (req, res) => {
-  List.find().then((lists) => {
-    res.send(lists);
-  })
+    List.find().then((lists) => {
+        res.send(lists);
+    })
 });
 
 //Creating a new list
 app.post('/lists', (req, res) => {
-  let title = req.body.title;
+    let title = req.body.title;
 
-  let newList = new List({
-    title
-  });
-  newList.save().then((listDoc) => {
-    res.send(listDoc);
-  });
+    let newList = new List({
+        title
+    });
+    newList.save().then((listDoc) => {
+        res.send(listDoc);
+    });
 });
 
 //Updating a list (using id)
 app.patch('/lists/:id', (req, res) => {
-  List.findByIdAndUpdate({_id: req.params.id}, {
-    $set: req.body
-  }).then(() => {
-    res.sendStatus(200);
-  })
+    List.findOneAndUpdate({_id: req.params.id}, {
+        $set: req.body
+    }).then(() => {
+        res.sendStatus(200);
+    })
 });
 
 //Deleting the list with corresp. id
 app.delete('/lists/:id', (req, res) => {
-  List.findOneAndRemove({_id: req.params.id}).then((removedListDoc) => {
-    res.send(removedListDoc);
-  })
+    List.findOneAndRemove({_id: req.params.id}).then((removedListDoc) => {
+        res.send(removedListDoc);
+    })
 });
 
 //Getting all tasks that belong to list with corresp. id
 app.get('/lists/:listId/tasks', (req, res) => {
-  Task.find({
-    _listId: req.params.listId
-  }).then((tasks) => {
-    res.send(tasks);
-  });
+    Task.find({
+        _listId: req.params.listId
+    }).then((tasks) => {
+        res.send(tasks);
+    });
 });
 
 //Getting ONE task with stated id in corresp. list
-app.get('/lists/:listId/tasks/:taskId',(req,res)=>{
-  Task.findOne({
-    _listId: req.params.listId,
-    _id: req.params.taskId
-  }).then((task)=>{
-    res.send(task);
-  });
+app.get('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOne({
+        _listId: req.params.listId,
+        _id: req.params.taskId
+    }).then((task) => {
+        res.send(task);
+    });
 });
 
 //Creating new task
 app.post('/lists/:listId/tasks', (req, res) => {
-  let newTask = new Task({
-    title: req.body.title,
-    _listId: req.params.listId
-  });
-  newTask.save().then((newTaskDoc) => {
-    res.send(newTaskDoc);
-  });
+    let newTask = new Task({
+        title: req.body.title,
+        _listId: req.params.listId
+    });
+    newTask.save().then((newTaskDoc) => {
+        res.send(newTaskDoc);
+    });
 });
 
 //Updating task by id
 app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
-  Task.findByIdAndUpdate({
-    _id: req.params.taskId,
-    _listId: req.params.listId
-  }, {
-    $set: req.body
-  }).then(() => {
-    res.sendStatus(200);
-  });
+    Task.findOneAndUpdate({
+            _id: req.params.taskId,
+            _listId: req.params.listId
+        }, {
+            $set: req.body
+        }
+    ).then(() => {
+        res.send({ message: 'Updated successfully.' })
+    })
 });
 
 //Deleting task by id
 app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
-  Task.findOneAndRemove({
-    _id: req.params.taskId,
-    _listId: req.params.listId
-  }).then((removedTaskDoc) => {
-    res.send(removedTaskDoc);
-  });
+    Task.findOneAndRemove({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }).then((removedTaskDoc) => {
+        res.send(removedTaskDoc);
+    });
 });
 
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+    console.log("Server is listening on port 3000");
 });
