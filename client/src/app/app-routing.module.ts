341991a12a0c975373411665be9cd650a5f0fde7
apocalypse-suite <mitchenko.catherine@gmail.com>
2021-02-13 13:43:30 +0200

    routing for login set

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {TaskViewComponent} from './pages/task-view/task-view.component';
 import {NewFolderComponent} from './pages/new-folder/new-folder.component';
 import {NewTaskComponent} from './pages/new-task/new-task.component';
+import {LoginComponent} from './pages/login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/lists', pathMatch: 'full'},
   {path: 'new-folder', component: NewFolderComponent},
   {path: 'lists', component: TaskViewComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'lists/:listId', component: TaskViewComponent},
   {path: 'lists/:listId/new-task', component: NewTaskComponent}
 ];
